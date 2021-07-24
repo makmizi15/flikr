8377bd7433bf05256df50d7929e5a295bcb970d0
Chris Pham <Chris@Dennes-MacBook-Pro.local>
2021-07-24 14:47:25 -0700

    add index router

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+//required modules
 const express = require('express');
 const logger = require('morgan');
 const PORT = 4000;
-
-require('dotenv').config()
-
+const indexRouter = require('./routes/index');
+// require('dotenv').config()
+//setup express ap
 const app = express();
-
+//configure express app
 app.set('view engine', 'ejs');
+
+// Mount middleware app.use()
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', indexRouter);
 
 app.listen(PORT, console.log(`Our server is live on http://localhost:${PORT}`));
