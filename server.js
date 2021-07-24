35f6aef2c7d67991c6863a58c0a367c40255ee34
Chris Pham <Chris@Dennes-MacBook-Pro.local>
2021-07-24 15:29:54 -0700

    add passport.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 //required modules
 const express = require('express');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 const PORT = 4000;
 const indexRouter = require('./routes/index');
-// require('dotenv').config()
-//setup express ap
+require('dotenv').config()
+require('./config/passport');
+
+//setup express app
 const app = express();
 //configure express app
 app.set('view engine', 'ejs');
@@ -13,7 +17,16 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: "Flikr",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', indexRouter);
 
 app.listen(PORT, console.log(`Our server is live on http://localhost:${PORT}`));
