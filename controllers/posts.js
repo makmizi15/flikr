56432356782849484014467af38978bdbb1d1076
Chris Pham <Chris@Dennes-MacBook-Pro.local>
2021-07-31 12:34:45 -0700

    add delete function

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const mongoose = require('mongoose');
+const post = new Post;
 
 module.exports = {
     create: createPost,
@@ -16,20 +18,64 @@ function addComment(req, res) {
     Post.findById(req.params.id, function (err, post) {
         post.comments.push(req.body);
         post.save(function(err) {
+            
             res.redirect('back');
         });
     });
 };
 
 
-function deleteComment(req, res) {
-    Post.findByIdAndDelete(req.params.id, function (err, post) {
-        if (err) console.log(err);
-        res.redirect('back');
+// function deleteComment(req, res) {
+//     console.log('delete METHOD!')
+
+//     console.log(req.params.id);
+//     // let id = mongoose.Types.ObjectId(req.params.id);
+//     post.comments.pull(req.params.id);
+//     console.log(foundPost);
+//     post.save(function(err) {
+//         if(err) return handleError(err);
+//         console.log('ReMOVED');
+       
+//     })
+//         res.redirect('back')
+    
+//     // Post.findByIdAndDelete({"comments.id": id}, function (err, post) { 
+//     //     if (err) console.log(err);
+//     //     console.log(post.comments);
+//     //     // const comment = post.comments.id(_id);
+//     //     // console.log(comment);
+//     //     // console.log(post);
+//     //     // post.save(function(err) {
+//     //     //     if (err) console.log(err);
+//     //     // })
+//     //     res.redirect('back');
+//     // });
+
+// };
+
+function deleteComment (req, res) {
+    Post.comments.findByIdAndDelete(req.params.id, function (err, deletedComment) {
+        if (err) return res.send(err);
+       comment.findById(deletedComment, function(err, post){
+            post.comment.remove(deletedComment);
+            post.save();
+            return res.redirect("back");
+            
+        });
     });
-
 };
-
+// function deleteComment(req,res) {
+//     console.log('delete method');
+//     console.log(req.params.id);
+//     Post.findById(req.params.id, function ( err, post) {
+//         if (err) {
+//             return console.log(err);
+//         };
+//         post.comments.remove(function(err) {
+//             console.log(post.comments);
+//         });
+//     });
+// };
 
 function editComment(req, res, next) {
     Post.findByIdAndUpdate(req.params.id, function (err, post) {
